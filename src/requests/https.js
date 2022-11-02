3fe023830d9d832953dad5a93b5f3238134feb8a
mochen <2827075398@qq.com>
2022-11-02 19:11:25 +0800

    完成测试，首次提交

---
+++ src/requests/https.js
@@ -0,0 +1,34 @@
+//引入 axios
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: "/api",
+    timeout: 50000,
+});
+
+// 数据请求拦截
+http.interceptors.request.use(
+    (config) => {
+        console.log(config);
+        // config.headers["Content-Type"] = "application/x-www-form-urlencoded";
+        config.headers["Content-Type"] = "application/json";
+        config.headers.Token = "";
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// 返回响应数据拦截
+http.interceptors.response.use(
+    (res) => {
+        const data = res.data;
+        return Promise.resolve(data);
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+//这是一位大佬指点的方法，更加简单
+export default http;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    transpileDependencies: true,
+    devServer: {
+        open: false,
+        host: "0.0.0.0",
+        port: 8080,
+        hot: true,
+        proxy: {
+            "/mov": {
+                target: "http://127.0.0.1:8080", // 你请求的第三方接口
+                changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+                pathRewrite: {
+                    // 路径重写，
+                    "^/api": "", // 替换target中的请求地址，也就是说以后你在请求http://api.wpbom.com这个地址的时候直接写成/Api即可。
+                },
+            },
+        },
+    },
+});
